8db1e03eb1008fa73cc96a74a35edc0b71da4a7e
Andres Lora <jalc.andres.lora@gmail.com>
2023-11-28 12:21:42 -0700

    Added .env configuration file to hide important URL and Passwords

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,35 @@
 const http = require('http');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 const server = http.createServer(app);
-mongoose.connection.on('open', () => {
+dotenv.config();
+/* mongoose.connection.on('open', () => {
   console.log('MongoDB connection ready !!!');
 });
 mongoose.connection.on('error', (error) => {
   console.error(error);
-});
+}); */
+
+// database connection
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB database is connected');
+  } catch (error) {
+    console.log('MongoDB database is connected failed ', error);
+  }
+};
+
 const PORT = process.env.PORT || 8000;
-const MONGO_URL =
-  'mongodb+srv://nodedev123:NSNrpIZLeEU2BATc@cluster0.kat0cop.mongodb.net/?retryWrites=true&w=majority';
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  // await mongoose.connect(process.env.MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
+    connectDB();
     console.log('listening on port ' + PORT);
   });
 }
