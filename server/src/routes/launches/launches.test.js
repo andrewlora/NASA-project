50caf70358ec156a33a13510a2ccbabec813b9f1
Andres Lora <jalc.andres.lora@gmail.com>
2023-12-02 21:20:22 -0700

    Added /v1 to versioning the API. Changed client and server urls

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -12,7 +12,7 @@ describe('Launches API', () => {
   describe('Test GET /launches', () => {
     test('It should respond with 2000 success', async () => {
       const response = await request(app)
-        .get('/launches')
+        .get('/v1/launches')
 
         .expect('Content-Type', /json/)
         .expect(200);
@@ -40,7 +40,7 @@ describe('Launches API', () => {
     };
     test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(completeLaunchData)
         .expect('Content-Type', /json/)
         .expect(201);
@@ -51,7 +51,7 @@ describe('Launches API', () => {
     });
     test('It should catch missing required properties', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(launchDataWithoutDate)
         .expect('Content-Type', /json/)
         .expect(400);
@@ -61,7 +61,7 @@ describe('Launches API', () => {
     });
     test('It should catch invalid dates', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(launchDataWithInvalidDate)
         .expect('Content-Type', /json/)
         .expect(400);
