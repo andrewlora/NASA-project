1f8610be65d62928ddfe39b3f40abc653d174b69
Andres Lora <jalc.andres.lora@gmail.com>
2023-12-11 15:08:52 -0700

    Added OAuth2 with passport and passport-google-oauth2

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,27 @@ const cors = require('cors');
 const helmet = require('helmet');
 const express = require('express');
 const morgan = require('morgan');
+const passport = require('passport');
+require('dotenv').config();
+const { Strategy } = require('passport-google-oauth20');
 const api = require('./routes/api');
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+function verifyCallBack(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+}
+passport.use(new Strategy(AUTH_OPTIONS), verifyCallBack);
 const app = express();
 app.use(helmet());
+app.use(passport.initialize());
 app.use(
   cors({
     origin: 'https://localhost:3000',
